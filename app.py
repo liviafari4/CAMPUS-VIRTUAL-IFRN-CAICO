acae914da34d3b4096fedc4f9def36ee5a168ab3
Evelyn Ester <evelyn.ester@escolar.ifrn.edu.br>
2026-05-05 22:13:44 -0300

    páginas de cursos + melhorias (Evelyn)

--- app.py
+++ app.py
@@ -21,5 +21,28 @@ def cursos():
 def campus():
     return render_template('campus.html')
 
+<<<<<<< HEAD
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+@app.route('/curso/informatica')
+def curso_informatica():
+    return render_template('curso_informatica.html')
+
+@app.route('/curso/textil')
+def curso_textil():
+    return render_template('curso_textil.html')
+
+@app.route('/curso/vestuario')
+def curso_vestuario():
+    return render_template('curso_vestuario.html')
+
+@app.route('/curso/eletrotecnica')
+def curso_eletro():
+    return render_template('curso_eletrotecnica.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+>>>>>>> 399f637 (páginas de cursos + melhorias (Evelyn))
